505c3160add5129c5a30a6697fecaa4e98bd3efc
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-07-15 15:33:01 +0200

    Remove fit in identity transform

--- fmralign/methods/identity.py
+++ fmralign/methods/identity.py
@@ -4,11 +4,6 @@ from fmralign.methods.base import BaseAlignment
 class Identity(BaseAlignment):
     """Compute no alignment, used as baseline for benchmarks : RX = X."""
 
-    def fit(self, X, Y):
-        raise NotImplementedError(
-            "Identity method does not require fitting. Use transform directly."
-        )
-
     def transform(self, X):
         """Returns X"""
         return X
