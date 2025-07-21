db8c490c63104932d94f4f454e95d2d070550098
Pierre-Louis Barbarant <104081777+pbarbarant@users.noreply.github.com>
2025-07-21 14:29:10 +0200

    Update fmralign/methods/piecewise.py
    Co-authored-by: bthirion <bertrand.thirion@inria.fr>

--- fmralign/methods/piecewise.py
+++ fmralign/methods/piecewise.py
@@ -67,6 +67,7 @@ def _array_to_list(arr, labels):
 def _list_to_array(lst, labels):
     """
     Convert a list of arrays back to a 2D array based on labels.
+
     Parameters
     ----------
     lst : list of ndarray
