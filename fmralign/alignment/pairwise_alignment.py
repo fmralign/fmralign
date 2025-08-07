ecd48b465674641538bbeb59c4e75cd399026ee9
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-08-07 20:50:42 +0200

    Improve PairwiseAlignment docstring

--- fmralign/alignment/pairwise_alignment.py
+++ fmralign/alignment/pairwise_alignment.py
@@ -17,8 +17,9 @@ class PairwiseAlignment(BaseEstimator, TransformerMixin):
     Parameters
     ----------
     method : str or a `BaseAlignment` instance, default="identity"
-        The alignment method to use. Supported methods depend on the
-        underlying alignment implementation.
+        The alignment method to use. It can be a string representing the method name
+        or an instance of a class derived from `BaseAlignment`. Available methods include:
+        ["identity", "procrustes", "ot", "sparseuot", "ridge"].
     labels : array-like or None, default=None
         Describes each voxel label's in the case of non-overlapping parcels.
         If provided, local alignments can be performed in parallel.
