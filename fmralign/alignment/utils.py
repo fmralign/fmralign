a41cddafaa35ebc35f30fd0a673c734a3e05811f
Pierre-Louis Barbarant <104081777+pbarbarant@users.noreply.github.com>
2025-08-14 14:23:42 +0200

    Update fmralign/alignment/utils.py
    Co-authored-by: bthirion <bertrand.thirion@inria.fr>

--- fmralign/alignment/utils.py
+++ fmralign/alignment/utils.py
@@ -236,7 +236,7 @@ def _map_to_target(
         the whole brain method is used.
         If multiple labels are present, the method will patch the parcels estimators
         in a big whole brain estimator.
-    n_jobs : int, optional
+    n_jobs : int, default=1
         Number of jobs to run in parallel. If -1, all CPUs are used.
         If 1, no parallel computing code is used at all, by default 1
     verbose : int, optional
