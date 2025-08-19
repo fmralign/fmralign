c574c5ae3ae4332473f3edf8eafede449d14194f
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-08-19 14:05:59 +0200

    Remove parcellation test in utils

--- fmralign/tests/test_utils.py
+++ fmralign/tests/test_utils.py
@@ -1,59 +1,14 @@
 # -*- coding: utf-8 -*-
-import nibabel as nib
-import numpy as np
 import pytest
-from nilearn.maskers import NiftiMasker
 from numpy.testing import assert_array_equal
 from sklearn.exceptions import NotFittedError
 
 from fmralign import GroupAlignment
 from fmralign._utils import (
-    _make_parcellation,
     load_alignment,
     save_alignment,
 )
-from fmralign.tests.utils import random_niimg, sample_subjects
-
-
-def test_make_parcellation():
-    # make_parcellation is built on Nilearn which already
-    # has several test for its Parcellation class
-    # here we test just the call of the API is right on a simple example
-    img, mask_img = random_niimg((7, 6, 8, 5))
-    masker = NiftiMasker(mask_img=mask_img).fit()
-
-    methods = ["kmeans", "ward", "hierarchical_kmeans", "rena"]
-
-    for clustering_method in methods:
-        # check n_pieces = 1 gives out ones of right shape
-        assert (
-            _make_parcellation(img, clustering_method, 1, masker)
-            == masker.transform(mask_img)
-        ).all()
-
-        # check n_pieces = 2 find right clustering
-        labels = _make_parcellation(img, clustering_method, 2, masker)
-        assert len(np.unique(labels)) == 2
-
-        # check that not inputing n_pieces yields problems
-        with pytest.raises(Exception):
-            assert _make_parcellation(img, clustering_method, 0, masker)
-
-    clustering = nib.Nifti1Image(
-        np.hstack([np.ones((7, 3, 8)), 2 * np.ones((7, 3, 8))]), np.eye(4)
-    )
-
-    # check 3D Niimg clusterings
-    for n_pieces in [0, 1, 2]:
-        labels = _make_parcellation(img, clustering, n_pieces, masker)
-        assert len(np.unique(labels)) == 2
-
-    # check warning if a parcel is too big
-    with pytest.warns(UserWarning):
-        clustering = nib.Nifti1Image(
-            np.hstack([np.ones(2000), 4 * np.ones(800)]), np.eye(4)
-        )
-        _make_parcellation(img, clustering_method, n_pieces, masker)
+from fmralign.tests.utils import sample_subjects
 
 
 def test_saving_and_loading(tmp_path):
