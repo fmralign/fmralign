30a41100f38f0c8ef8d94d37681501e0cc53260b
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-08-21 16:29:42 +0200

    Add tests for group_alignment on all methods

--- fmralign/alignment/tests/test_group_alignment.py
+++ fmralign/alignment/tests/test_group_alignment.py
@@ -1,51 +1,76 @@
+import pytest
 from numpy.testing import assert_array_equal
 
 from fmralign import GroupAlignment
-from fmralign.methods import Identity
+from fmralign.alignment.utils import _check_method
 from fmralign.tests.utils import sample_subjects
 
+methods = ["identity", "ot", "sparse_uot", "procrustes", "ridge", "srm"]
 
-def test_alignment_template():
+
+@pytest.mark.parametrize("method", methods)
+def test_alignment_template(method):
     """Test template alignment."""
     subjects_data, labels = sample_subjects()
     X = dict(enumerate(subjects_data))
 
-    algo = GroupAlignment(labels=labels)
+    algo = GroupAlignment(method=method, labels=labels)
     algo.fit(X, y="template")
 
-    assert isinstance(algo.method_, Identity)
+    assert isinstance(algo.method_, _check_method(method).__class__)
     assert len(algo.fitted_estimators) == len(X)
-    assert algo.template.shape == X[0].shape
-    for i, x in enumerate(X):
+    if method != "srm":
+        assert algo.template.shape == X[0].shape
+    for i, x in X.items():
         transformed = algo._transform_one_array(x, algo.fitted_estimators[i])
-        assert_array_equal(transformed, x)
+        if method == "identity":
+            assert_array_equal(transformed, x)
+        elif method == "srm":
+            n_components = algo.fitted_estimators[0].W.shape[0]
+            assert transformed.shape == (x.shape[0], n_components)
+        else:
+            assert transformed.shape == x.shape
 
 
-def test_alignment_target():
+@pytest.mark.parametrize("method", methods)
+def test_alignment_target(method):
     """Test alignment to a target"""
     subjects_data, labels = sample_subjects()
     X = dict(enumerate(subjects_data))
 
     target = X[0]
-    algo = GroupAlignment(labels=labels)
+    algo = GroupAlignment(method=method, labels=labels)
     algo.fit(X, y=target)
 
-    assert isinstance(algo.method_, Identity)
+    assert isinstance(algo.method_, _check_method(method).__class__)
     assert len(algo.fitted_estimators) == len(X)
     assert algo.template is None
-    for i, x in enumerate(X):
+    for i, x in X.items():
         transformed = algo._transform_one_array(x, algo.fitted_estimators[i])
-        assert_array_equal(transformed, x)
+        if method == "identity":
+            assert_array_equal(transformed, x)
+        elif method == "srm":
+            n_components = algo.fitted_estimators[0].W.shape[0]
+            assert transformed.shape == (x.shape[0], n_components)
+        else:
+            assert transformed.shape == x.shape
 
 
-def test_transform():
+@pytest.mark.parametrize("method", methods)
+def test_transform(method):
     """Test transform method."""
     subjects_data, labels = sample_subjects()
     X = dict(enumerate(subjects_data))
 
-    algo = GroupAlignment(labels=labels)
+    algo = GroupAlignment(method=method, labels=labels)
     algo.fit(X)
 
     transformed_arrays = algo.transform(X)
     for i, x in X.items():
-        assert_array_equal(transformed_arrays[i], x)
+        if method == "identity":
+            assert_array_equal(transformed_arrays[i], x)
+        elif method == "srm":
+            n_components = algo.fitted_estimators[0].W.shape[0]
+            assert transformed_arrays[i].shape == (x.shape[0], n_components)
+        else:
+            assert transformed_arrays[i].shape == x.shape
