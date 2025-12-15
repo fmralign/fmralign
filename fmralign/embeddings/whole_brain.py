9e59620be830124cbe76bcd5cce1a9cbe5bda739
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-12-15 19:46:37 +0100

    Add laplacian embeddings

--- fmralign/embeddings/whole_brain.py
+++ fmralign/embeddings/whole_brain.py
@@ -1,6 +1,10 @@
 import numpy as np
+import robust_laplacian as rl
+import scipy.sparse.linalg as sla
+from nibabel import Nifti1Image
 from nilearn.image import resampling
 from nilearn.masking import load_mask_img
+from nilearn.surface import PolyMesh
 from sklearn import neighbors
 
 
@@ -34,3 +38,32 @@ def get_adjacency_from_mask(mask_img, radius):
     A = clf.fit(mask_coords).radius_neighbors_graph(mask_coords)
 
     return A.tocoo().astype(bool)
+
+
+def get_laplacian_embedding(mask_img, k, sigma=1e-8):
+    if isinstance(mask_img, Nifti1Image):
+        mask_data, _ = load_mask_img(mask_img)
+        V = np.array(np.nonzero(mask_data)).T
+        L, M = rl.point_cloud_laplacian(V)
+    else:
+        mesh = (
+            mask_img
+            if isinstance(mask_img, PolyMesh)
+            else PolyMesh(**mask_img)
+        )
+        V = np.vstack(
+            [
+                np.asarray(part.coordinates, dtype="<f4")
+                for part in mesh.parts.values()
+            ]
+        )
+        F = np.vstack(
+            [
+                np.asarray(part.faces, dtype="<f4")
+                for part in mesh.parts.values()
+            ]
+        )
+        L, M = L, M = rl.mesh_laplacian(V, F)
+
+    _, evecs = sla.eigsh(L, k, M, sigma=sigma)
+    return evecs.T
