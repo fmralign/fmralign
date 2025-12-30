a73134fbb6a8a5dac53273143aa2d3f3282f3f1e
Pierre-Louis Barbarant <plbarbarant@mail.com>
2025-12-30 18:25:59 +0100

    Add tests and docstring to get_laplacian_embedding

--- fmralign/embeddings/whole_brain.py
+++ fmralign/embeddings/whole_brain.py
@@ -41,29 +41,50 @@ def get_adjacency_from_mask(mask_img, radius):
 
 
 def get_laplacian_embedding(mask_img, k, sigma=1e-8):
+    """
+    Computes the Laplacian embedding of a given mask image or surface mesh.
+
+    Given a mask image (Nifti1Image) or a surface mesh (PolyMesh or InMemoryMesh),
+    this function computes the Laplace-Beltrami eigenfunctions on the vertices
+    defined by the mask or mesh. It solves a generalized eigenvalue problem to return
+    the first `k` eigenfunctions.
+
+    Parameters
+    ----------
+    mask_img : Nifti1Image or PolyMesh or InMemoryMesh
+        Input mask image or surface mesh.
+    k : int
+        Number of eigenfunctions to compute.
+    sigma : float, optional
+        Stability parameter passed to the sparse solver, by default 1e-8.
+
+    Returns
+    -------
+    evecs : ndarray of shape (k, n_vertices)
+        The computed Laplacian eigenfunctions.
+    """
     if isinstance(mask_img, Nifti1Image):
         mask_data, _ = load_mask_img(mask_img)
         V = np.array(np.nonzero(mask_data)).T
         L, M = rl.point_cloud_laplacian(V)
-    else:
-        mesh = (
-            mask_img
-            if isinstance(mask_img, PolyMesh)
-            else PolyMesh(**mask_img)
-        )
+    elif isinstance(mask_img, PolyMesh):
         V = np.vstack(
             [
                 np.asarray(part.coordinates, dtype="<f4")
-                for part in mesh.parts.values()
+                for part in mask_img.parts.values()
             ]
         )
         F = np.vstack(
             [
                 np.asarray(part.faces, dtype="<f4")
-                for part in mesh.parts.values()
+                for part in mask_img.parts.values()
             ]
         )
         L, M = L, M = rl.mesh_laplacian(V, F)
+    else:
+        V = np.asarray(mask_img.coordinates, dtype="<f4")
+        F = np.asarray(mask_img.faces, dtype="<f4")
+        L, M = rl.mesh_laplacian(V, F)
 
     _, evecs = sla.eigsh(L, k, M, sigma=sigma)
     return evecs.T
