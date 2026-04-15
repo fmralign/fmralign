d50ab762b446fef99bfb90f6399bf0100a5a9998
Pierre-Louis Barbarant <104081777+pbarbarant@users.noreply.github.com>
2026-04-15 14:48:02 +0200

    [FIX] Simplify OT by streamlining computations to POT (#186)
    * Use POT's new backend to simplify computations of OT
    
    * Remove fugw from toml
    
    * Modify request_mocker import (#188)
    
    Co-authored-by: Pierre-Louis Barbarant <plbarbarant@mail.com>
    
    * Save transport plan as np array
    
    * Convert R to tensor in transform
    
    ---------
    
    Co-authored-by: Pierre-Louis Barbarant <plbarbarant@mail.com>

---
+++ fmralign/methods/tests/test_optimal_transport.py
@@ -0,0 +1,33 @@
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+
+from fmralign.methods.optimal_transport import OptimalTransport
+
+
+def test_identity_wasserstein():
+    """Test that the optimal coupling matrix is the\n
+    identity matrix when using the identity alignment."""
+    n_samples, n_features = 10, 5
+    X = np.random.randn(n_samples, n_features)
+    algo = OptimalTransport(reg=1e-6)
+    algo.fit(X, X)
+    # Check if transport matrix P is uniform diagonal
+    assert_array_almost_equal(algo.R, np.eye(n_features) / n_features)
+    # Check if transformation preserves input
+    assert_array_almost_equal(X, algo.transform(X))
+
+
+def test_regularization_effect():
+    """Test the effect of regularization parameter."""
+    n_samples, n_features = 10, 5
+    X = np.random.randn(n_samples, n_features)
+
+    # Compare results with different regularization values
+    algo1 = OptimalTransport(reg=10.0)
+    algo2 = OptimalTransport(reg=1e-3)
+
+    algo1.fit(X, X)
+    algo2.fit(X, X)
+
+    # Higher regularization should lead to flatter diagonal
+    assert np.all(np.diag(algo1.R) < np.diag(algo2.R))
