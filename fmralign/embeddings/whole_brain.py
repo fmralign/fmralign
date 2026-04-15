fd80e02875e12a1e5f57e6d055873ced9bfd56d2
Pierre-Louis Barbarant <plbarbarant@mail.com>
2026-04-15 16:15:39 +0200

    Add get_laplacian_embedding

--- fmralign/embeddings/whole_brain.py
+++ fmralign/embeddings/whole_brain.py
@@ -1,6 +1,10 @@
 import numpy as np
+import robust_laplacian as rl
+import scipy.sparse.linalg as sla
+from nibabel import Nifti1Image
 from nilearn.image import resampling
 from nilearn.masking import load_mask_img
+from nilearn.surface import InMemoryMesh
 from sklearn import neighbors
 
 
@@ -34,3 +38,44 @@ def get_adjacency_from_mask(mask_img, radius):
     A = clf.fit(mask_coords).radius_neighbors_graph(mask_coords)
 
     return A.tocoo().astype(bool)
+
+
+def get_laplacian_embedding(mask_img, k, sigma=1e-8):
+    """
+    Computes the Laplacian embedding of a given mask image or surface mesh.
+
+    Given a mask image (Nifti1Image) or a surface mesh (InMemoryMesh),
+    this function computes the Laplace-Beltrami eigenmodes on the vertices
+    defined by the mask or mesh. It solves a generalized eigenvalue problem to return
+    the first `k` eigenmodes.
+
+    Note: The zero-order eigenmode is skipped.
+
+    Parameters
+    ----------
+    mask_img : Nifti1Image or InMemoryMesh
+        Input mask image or surface mesh.
+    k : int
+        Number of eigenmodes to compute.
+    sigma : float, optional
+        Stability parameter passed to the sparse solver, by default 1e-8.
+
+    Returns
+    -------
+    evecs : ndarray of shape (k, n_vertices)
+        The computed Laplacian eigenmodes.
+    """
+    if isinstance(mask_img, Nifti1Image):
+        mask_data, _ = load_mask_img(mask_img)
+        V = np.array(np.nonzero(mask_data)).T
+        L, M = rl.point_cloud_laplacian(V)
+    elif isinstance(mask_img, InMemoryMesh):
+        V = np.asarray(mask_img.coordinates, dtype="<f4")
+        F = np.asarray(mask_img.faces, dtype="<f4")
+        L, M = rl.mesh_laplacian(V, F)
+    else:
+        raise ValueError("mask_img must be a Nifti1Image or an InMemoryMesh.")
+
+    _, evecs = sla.eigsh(L, k + 1, M, sigma=sigma)
+
+    return evecs.T[1:]  # skip first eigenvector
