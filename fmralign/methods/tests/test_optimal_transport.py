bc1055e5a73a05ab1007e5d94c20a2110ed7ecaf
Pierre-Louis Barbarant <plbarbarant@mail.com>
2026-07-06 12:37:50 -0700

    Update spectral OT test with geomloss backend

--- fmralign/methods/tests/test_optimal_transport.py
+++ fmralign/methods/tests/test_optimal_transport.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from numpy.testing import assert_array_almost_equal
 
 from fmralign.methods.optimal_transport import OptimalTransport, SpectralOT
@@ -33,23 +34,35 @@ def test_regularization_effect():
     assert np.all(np.diag(algo1.R) < np.diag(algo2.R))
 
 
-def test_spectral_ot():
+@pytest.mark.parametrize("backend", ["pot", "geomloss"])
+def test_spectral_ot(backend):
     """Test SpectralOT against OptimalTransport on synthetic data."""
     n_samples, n_features = 10, 5
-    X = np.random.randn(n_samples, n_features)
-    Y = X + 0.1 * np.random.randn(n_samples, n_features)
+    X_train = np.random.randn(n_samples, n_features)
+    Y_train = X_train + 0.1 * np.random.randn(n_samples, n_features)
+    X_test = np.random.randn(n_samples, n_features)
 
     evecs = np.random.randn(3, n_features)
 
     # Check the anatomical case (alpha=1.0)
     algo_ot = OptimalTransport(reg=1e-3)
-    algo_spectral_anat = SpectralOT(reg=1e-3, evecs=evecs, alpha=1.0)
+    algo_spectral_anat = SpectralOT(
+        reg=1e-3, evecs=evecs, alpha=1.0, backend=backend
+    )
     algo_ot.fit(evecs, evecs)
-    algo_spectral_anat.fit(X, Y)
-    assert_array_almost_equal(algo_ot.R, algo_spectral_anat.R)
+    algo_spectral_anat.fit(X_train, Y_train)
+    assert_array_almost_equal(
+        algo_ot.transform(X_test), algo_spectral_anat.transform(X_test)
+    )
 
     # Check the functional case (alpha=0.0)
-    algo_ot.fit(X, Y)
-    algo_spectral_func = SpectralOT(reg=1e-3, evecs=evecs, alpha=0.0)
-    algo_spectral_func.fit(X, Y)
-    assert_array_almost_equal(algo_ot.R, algo_spectral_func.R)
+    algo_ot.fit(X_train, Y_train)
+    algo_spectral_func = SpectralOT(
+        reg=1e-3, evecs=evecs, alpha=0.0, backend=backend
+    )
+    algo_spectral_func.fit(X_train, Y_train)
+    assert_array_almost_equal(
+        algo_ot.transform(X_test),
+        algo_spectral_func.transform(X_test),
+        decimal=5,
+    )
